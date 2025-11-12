368f26fa0094dcbf2eedfb0de8f43e2a904bf357
Ellie Saga <elliesaga03@gmail.com>
2025-11-12 10:46:18 +0000

    Plotted results of resolved GVD in test file

--- testcode.py
+++ testcode.py
@@ -2,16 +2,31 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import resource.pulseshape as pulse
+import resource.operators as op
 import math
-def basicGVD(dispParameter,centFrequency,stepsize):
-    #first get value of the basic GVD operator - basic NLSE
-    exponent = 2*(np.pi**2)*dispParameter*(centFrequency**2)*stepsize
-    exponent = 0 + (exponent*1j)
-    return np.exp(exponent)
+
 
 #testing GVD script
-b2 = 1 #in ps^2m^-1
+b2 = -5 #in ps^2m^-1
 nu = 300
+stepsize = 5
+
+t = np.linspace(-10,10,1024)
+gaussPulseInitial = pulse.GaussianPulse(t,1)
+#apply gvd to test pulse once
+gaussPulse = op.resolveGVD(b2,stepsize,gaussPulseInitial)
+
+for i in range(1000):
+    gaussPulse = op.resolveGVD(b2,stepsize,gaussPulse)
+
+
+plt.plot(t,np.square(gaussPulseInitial),label = "input")
+plt.plot(t,np.square(np.abs(gaussPulse)),label = "after GVD")
+plt.legend(loc="best")
+plt.ylabel(r"$I(T)$")
+plt.xlabel(r"$T$")
+plt.show()
+
 
 
 
