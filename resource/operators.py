2156c4923b9dc74cd521602e7b5088380ef84a61
Ellie Saga <elliesaga03@gmail.com>
2025-11-14 12:03:06 +0000

    Moved runge kutta function to operators file to resolve circular import

--- resource/operators.py
+++ resource/operators.py
@@ -23,3 +23,18 @@ def SymSplitStepNL(gamma,pulse,stepsize):
     return np.exp(exponent)
 def BasicRKNL(gamma,pulse,stepsize):
     return gamma*np.square(np.abs(pulse))*stepsize*(1j)
+
+def BasicRK4IP(pulse,b2,gamma,stepSize,samplingRate):
+    #get k1-k4 parts
+    PulseIP = resolveBasicGVD(b2,stepSize,pulse,samplingRate)
+    k1 = resolveBasicGVD(b2,stepSize,np.multiply(pulse,BasicRKNL(gamma,pulse,stepSize)))
+    k2 = np.multiply(BasicRKNL(gamma,np.add(PulseIP,np.divide(k1,2)),stepSize),np.add(PulseIP,np.divide(k1,2)))
+    k3 = np.multiply(BasicRKNL(gamma,np.add(PulseIP,np.divide(k2,2)),stepSize),np.add(PulseIP,np.divide(k2,2)))
+    k4 = np.multiply(BasicRKNL(gamma,resolveBasicGVD(b2,stepSize,np.add(PulseIP,k3),samplingRate),stepSize),resolveBasicGVD(b2,stepSize,np.add(PulseIP,k3),samplingRate))
+    #sum parts together
+    s1 = np.add(PulseIP,np.divide(k1,6))
+    s2 = np.add(s1,np.divide(k2,3))
+    s3 = np.add(s2,np.divide(k3,3))
+    #final step
+    step = np.add(np.divide(k4,6),resolveBasicGVD(b2,stepSize,s3,samplingRate))
+    return step
