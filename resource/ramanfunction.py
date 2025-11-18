9fa33c7c9055d0685d0b60313d4bb6989dabe681
Ellie Saga <elliesaga03@gmail.com>
2025-11-18 11:10:55 +0000

    added clarifying comment

--- resource/ramanfunction.py
+++ resource/ramanfunction.py
@@ -1,6 +1,7 @@
 #raman function generation used in code
 import numpy as np
 def BlowWoodResponse(maxTime,timeSteps,t1,t2):
+    #NOTE: Sampling rates between raman function and pulse MUST match
     t = np.linspace(0,maxTime,timeSteps)
     response = ((t1**2)+(t2**2))/(t1*(t2**2))
     response = np.multiply(response,np.exp(-(t/t2)))
