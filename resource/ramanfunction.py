a114c2a8a8061c56969dc23d3ba7d82c33b96806
Ellie Saga <elliesaga03@gmail.com>
2025-11-19 11:45:47 +0000

    added default t1 and t2 values

--- resource/ramanfunction.py
+++ resource/ramanfunction.py
@@ -1,6 +1,6 @@
 #raman function generation used in code
 import numpy as np
-def BlowWoodResponse(maxTime,timeSteps,t1,t2):
+def BlowWoodResponse(maxTime,timeSteps,t1=12.2,t2=32):
     #NOTE: Sampling rates between raman function and pulse MUST match
     t = np.linspace(0,maxTime,timeSteps)
     response = ((t1**2)+(t2**2))/(t1*(t2**2))
