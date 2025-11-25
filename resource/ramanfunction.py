4de29b6dcc4f752d996db458d0343c8442d347b8
Ellie Saga <elliesaga03@gmail.com>
2025-11-25 09:35:21 +0000

    Created new improved response function

--- resource/ramanfunction.py
+++ resource/ramanfunction.py
@@ -7,3 +7,11 @@ def BlowWoodResponse(maxTime,timeSteps,t1=12.2,t2=32):
     response = np.multiply(response,np.exp(-(t/t2)))
     response = np.multiply(response,np.sin((t/t1)))
     return t,response
+
+def BlowWoodResponse2(time,t1=12.2,t2=32):
+    response = ((t1**2)+(t2**2))/(t1*(t2**2))
+    response = np.multiply(response,np.exp(-(time/t2)))
+    response = np.multiply(response,np.sin((time/t1)))
+    #same as before, except now we make all values of response for negative t equal to 0
+    response = [r if time[i] >= 0 else 0 for i,r in enumerate(response)]
+    return response 
